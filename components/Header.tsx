785542aec41ec332e1792c8720f79a98b69f9990
SelcukCaliskan <selcukcaliskan98@gmail.com>
2024-07-31 01:05:27 +0300

    Add Header component and update Home page

---
+++ components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <header>
+      <h1>My Next.js App</h1>
+      <nav>
+        <a href='/'>Home</a>
+        <a href='/about'>About</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
