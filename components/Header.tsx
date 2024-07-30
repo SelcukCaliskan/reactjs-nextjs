f7c95ae9547269b2ed8e103a04d3d9669129676f
SelcukCaliskan <selcukcaliskan98@gmail.com>
2024-07-31 02:13:59 +0300

    Changed Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 const Header: React.FC = () => {
   return (
+    <div>
     <header>Header Content</header>
+    <p>Burada bilerek iki farklı brancte hata oluşturuldu.</p>
+    </div>
   );
 };
 
